29d8ae8c5057974383bfcae67bb1693bd1726d85
jimzord12 <jimzordgtx@gmail.com>
2026-03-11 20:48:26 +0200

    feat: Set up React testing environment with Jest and React Testing Library.

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,13 @@
 /** @type {import('jest').Config} */
 const config = {
+  rootDir: './',
+  preset: '@swc/jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/test/jest.setup.ts'],
+  moduleNameMapper: {
+    '\.(css|less|scss)$': 'identity-obj-proxy',
+    '\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/test/mocks/fileMock.js',
+  },
   // Map both .js and .ts files (with or without React/JSX) to SWC
   transform: {
     '^.+\\.(t|j)sx?$': '@swc/jest',
